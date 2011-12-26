ce059e0c614621d00e7e06172a2f5e920129332c
xingniu <oxstar@users.noreply.github.com>
2011-12-26 12:22:36 +0000

    Clean up

--- src/me/zhishi/tools/SmallTools.java
+++ src/me/zhishi/tools/SmallTools.java
@@ -6,7 +6,7 @@ public class SmallTools
 {
 	public static void main( String[] args ) throws IOException
 	{
-		generateBZ2List( URICenter.source_name_hudong);
+		generateBZ2List( URICenter.source_name_hudong );
 	}
 
 	public static void generateBZ2List( String source ) throws IOException
@@ -17,16 +17,16 @@ public class SmallTools
 		else if( source.equals( URICenter.source_name_baidu ) )
 			maxDump = Path.baiduMax;
 		
-		Path p = new Path( 3.0, source );
-		HDFSFileWriter fileWriter = new HDFSFileWriter( p.getHDFSMainPagePath() + "FileList.txt", false );
+		Path p = new Path( 3.0, source, true );
+		HDFSFileWriter fileWriter = new HDFSFileWriter( p.getMainPagePath() + "FileList.txt", false );
 		
 		for( int i = 0; i <= maxDump; ++i )
 		{
 			String archiveName = Integer.toString( i * 10000 + 1 );
 			archiveName += "-";
 			archiveName += Integer.toString( (i+1) * 10000 );
-			System.out.println( p.getHDFSMainPagePath() + archiveName + ".tar.bz2" );
-			fileWriter.writeLine( p.getHDFSMainPagePath() + archiveName + ".tar.bz2" );
+			System.out.println( p.getMainPagePath() + archiveName + ".tar.bz2" );
+			fileWriter.writeLine( p.getMainPagePath() + archiveName + ".tar.bz2" );
 		}
 		
 		fileWriter.close();
