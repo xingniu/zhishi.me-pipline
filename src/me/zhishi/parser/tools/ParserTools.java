6d9cf8420c4458a8be023447f70d7d56231a3128
xingniu <oxstar@users.noreply.github.com>
2012-08-14 12:16:07 +0000

    Add StreamingFileList

--- src/me/zhishi/parser/tools/ParserTools.java
+++ src/me/zhishi/parser/tools/ParserTools.java
@@ -3,7 +3,6 @@ package me.zhishi.parser.tools;
 import me.zhishi.tools.Path;
 import me.zhishi.tools.URICenter;
 import me.zhishi.tools.file.GZIPFileWriter;
-import me.zhishi.tools.file.HDFSFileWriter;
 import me.zhishi.tools.file.NTriplesReader;
 import me.zhishi.tools.file.TripleReader;
 import me.zhishi.tools.file.TripleWriter;
@@ -12,7 +11,6 @@ public class ParserTools
 {
 	public static void main( String[] args ) throws Exception
 	{
-//		generateBZ2List( URICenter.source_name_hudong );
 		superviseNTs( URICenter.source_name_hudong, 3.0, "label" );
 	}
 	
@@ -43,28 +41,5 @@ public class ParserTools
 		ntReader.close();
 		writer.close();
 		System.out.println( counter );
-	}
-
-	public static void generateBZ2List( String source )
-	{
-		int maxDump = 0;
-		if( source.equals( URICenter.source_name_hudong ) )
-			maxDump = Path.hudongMax;
-		else if( source.equals( URICenter.source_name_baidu ) )
-			maxDump = Path.baiduMax;
-		
-		Path p = new Path( 3.0, source, true );
-		HDFSFileWriter fileWriter = new HDFSFileWriter( p.getMainPageFolder() + "FileList.txt", false );
-		
-		for( int i = 0; i <= maxDump; ++i )
-		{
-			String archiveName = Integer.toString( i * 10000 + 1 );
-			archiveName += "-";
-			archiveName += Integer.toString( (i+1) * 10000 );
-			System.out.println( p.getMainPageFolder() + archiveName + ".tar.bz2" );
-			fileWriter.writeLine( p.getMainPageFolder() + archiveName + ".tar.bz2" );
-		}
-		
-		fileWriter.close();
 	}
 }
