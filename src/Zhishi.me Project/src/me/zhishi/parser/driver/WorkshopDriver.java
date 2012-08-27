8975abb7e9d97a1205f25f7c99d0a3a0e1460522
xingniu <xingniu@84c83138-e18d-4429-9777-468411ae1c88>
2012-08-27 13:48:25 +0000

    Refine code

--- src/Zhishi.me Project/src/me/zhishi/parser/driver/WorkshopDriver.java
+++ src/Zhishi.me Project/src/me/zhishi/parser/driver/WorkshopDriver.java
@@ -1,6 +1,8 @@
 package me.zhishi.parser.driver;
 
 import me.zhishi.parser.workshop.InfoboxRefiner;
+import me.zhishi.parser.workshop.PreprocessDBpedia;
+import me.zhishi.parser.workshop.SortByPredicate;
 import me.zhishi.tools.URICenter;
 
 public class WorkshopDriver
@@ -8,14 +10,16 @@ public class WorkshopDriver
 	public static void main( String[] args ) throws Exception
 	{
 		double releaseVersion;
-		String source;
 		
-		releaseVersion = 3.0;
-		source = URICenter.source_name_hudong;
-//		InfoboxRefiner.output( source, releaseVersion );
-//		InfoboxRefiner.run( source, releaseVersion );
-		source = URICenter.source_name_baidu;
-//		InfoboxRefiner.output( source, releaseVersion );
-//		InfoboxRefiner.run( source, releaseVersion );
+//		releaseVersion = 3.0;
+//		SortByPredicate.run( URICenter.source_name_hudong, releaseVersion );
+//		SortByPredicate.run( URICenter.source_name_baidu, releaseVersion );
+		
+//		releaseVersion = 3.1;
+//		PreprocessDBpedia.run( releaseVersion );
+		
+//		releaseVersion = 3.0;
+//		InfoboxRefiner.run( URICenter.source_name_hudong, releaseVersion );
+//		InfoboxRefiner.run( URICenter.source_name_baidu, releaseVersion );
 	}
 }
